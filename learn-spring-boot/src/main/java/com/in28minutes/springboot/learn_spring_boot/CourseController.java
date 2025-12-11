06cb13e29e84b7c237238d5f9416a416aa9ffd3d
Youngsik Yoon <dudtlr6512@naver.com>
2025-12-11 21:42:06 +0900

    first commit

---
+++ learn-spring-boot/src/main/java/com/in28minutes/springboot/learn_spring_boot/CourseController.java
@@ -0,0 +1,26 @@
+package com.in28minutes.springboot.learn_spring_boot;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// /courses
+// Course: id, name, author
+
+@RestController
+public class CourseController {
+	
+	// /courses
+	// Course: id, name, author
+	
+	@RequestMapping("/courses")
+	public List<Course> retrieveAllCourses() {
+		return Arrays.asList(
+				new Course(1, "Learn AWS", "in29minutes"),
+				new Course(2, "Learn DevOps", "in29minutes")
+				);
+	}
+
+}
